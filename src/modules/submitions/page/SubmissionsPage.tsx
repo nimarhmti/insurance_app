eec5ac7c5f6b23650888d89f20fae7a65cb69798
nimarhmti <nimarahmati2000@gmail.com>
2025-03-09 18:13:25 +0330

    fea-add some skeltone to the app

--- src/modules/submitions/page/SubmissionsPage.tsx
+++ src/modules/submitions/page/SubmissionsPage.tsx
@@ -1,43 +1,5 @@
-import DynamicTable from "../../../components/table/components/DynamicTabel";
-
-const mockData = {
-  columns: ["FullName", "Age", "Gender", "InsuranceType", "City"],
-  data: [
-    {
-      id: "1",
-      FullName: "John Doe",
-      Age: 28,
-      Gender: "Male",
-      InsuranceType: "Health",
-      City: "New York",
-    },
-    {
-      id: "2",
-      FullName: "Jane Smith",
-      Age: 32,
-      Gender: "Female",
-      InsuranceType: "Home",
-      City: "Los Angeles",
-    },
-    {
-      id: "3",
-      FullName: "Alice Brown",
-      Age: 40,
-      Gender: "Female",
-      InsuranceType: "Car",
-      City: "Chicago",
-    },
-  ],
-};
+import SubmissionContainer from "../components/submission-container";
 
 export default function SubmissionsPage() {
-  return (
-    <div>
-      <DynamicTable
-        pageSize={5}
-        columns={mockData.columns}
-        data={mockData.data}
-      />
-    </div>
-  );
+  return <SubmissionContainer />;
 }
